8a3c1f26721da4f89873ad902a7aa1bf4305ecce
Tung Du <dinhtungdu@gmail.com>
2022-08-31 17:34:06 +0700

    update stress test

--- tests/e2e/specs/shopper/product-search.test.ts
+++ tests/e2e/specs/shopper/product-search.test.ts
@@ -10,36 +10,38 @@ const block = {
 	class: '.wp-block-search',
 };
 
-describeOrSkip( GUTENBERG_EDITOR_CONTEXT === 'gutenberg' )(
-	`Shopper → Product Search`,
-	() => {
-		beforeEach( async () => {
-			await shopper.block.goToBlockPage( block.name );
-		} );
-
-		it( 'should render product variation', async () => {
-			await expect( page ).toMatchElement(
-				'input[name="post_type"][value="product"]'
-			);
-		} );
-
-		it( 'should be able to search for products', async () => {
-			await page.type( '.wp-block-search input[name="s"]', 'Stick' );
-
-			await Promise.all( [
-				page.keyboard.press( 'Enter' ),
-				page.waitForNavigation(),
-			] );
-
-			await expect( page ).toMatchElement( 'ul.products.columns-3' );
-
-			const products = await page.$$( 'ul.products.columns-3 > li' );
-
-			expect( products ).toHaveLength( 2 );
-
-			expect( page ).toMatchElement( 'ul.products.columns-3 > li', {
-				text: '128GB USB Stick',
+for ( let i=1; i<=100; i++ ) {
+	describeOrSkip( GUTENBERG_EDITOR_CONTEXT === 'gutenberg' )(
+		`Shopper → Product Search - ${i}`,
+		() => {
+			beforeEach( async () => {
+				await shopper.block.goToBlockPage( block.name );
 			} );
-		} );
-	}
-);
+
+			it( 'should render product variation', async () => {
+				await expect( page ).toMatchElement(
+					'input[name="post_type"][value="product"]'
+				);
+			} );
+
+			it( 'should be able to search for products', async () => {
+				await page.type( '.wp-block-search input[name="s"]', 'Stick' );
+
+				await Promise.all( [
+					page.keyboard.press( 'Enter' ),
+					page.waitForNavigation(),
+				] );
+
+				await expect( page ).toMatchElement( 'ul.products.columns-3' );
+
+				const products = await page.$$( 'ul.products.columns-3 > li' );
+
+				expect( products ).toHaveLength( 2 );
+
+				expect( page ).toMatchElement( 'ul.products.columns-3 > li', {
+					text: '128GB USB Stick',
+				} );
+			} );
+		}
+	);
+}
